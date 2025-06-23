c4efa90420c407aa42715ee5e0dd9ce2664f49b4
Sunnyhitha-27 <sunnyhithakasarla27@gmail.com>
2025-06-23 17:59:09 +0530

    Add files via upload

---
+++ html12/apex_planet_task5-main/script.js
@@ -0,0 +1,10 @@
+// Add form submission behavior
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Message sent! (Simulated)");
+});
+
+// Lazy loading demo – optional custom JS
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Portfolio loaded and optimized!");
+});
